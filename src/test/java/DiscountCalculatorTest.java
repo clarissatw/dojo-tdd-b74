ed95ad7b93bb849b37927710744b73ec99994f76
Jamille Bittencourt <jamille.bittencourt@thoughtworks.com>
2021-07-26 14:57:59 -0300

    changes variable to use inside test method

--- src/test/java/DiscountCalculatorTest.java
+++ src/test/java/DiscountCalculatorTest.java
@@ -1,25 +1,16 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DiscountCalculatorTest {
 
-    private DiscountCalculator discountCalculator;
-
-    @BeforeEach
-    public void setup(){
-        discountCalculator = new DiscountCalculator();
-    }
-
     @Test
-    public void shouldReturnOkWhenApplicationIsReady(){
+    public void shouldReturnOkWhenApplicationIsReady() {
         String expected = "Ok";
 
+        DiscountCalculator discountCalculator = new DiscountCalculator();
         String actual = discountCalculator.healthCheck();
 
         assertEquals(expected, actual);
-
     }
-
 }
